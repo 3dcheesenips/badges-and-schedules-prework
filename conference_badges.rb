9421ffe752b1c1b6d6b77bf19c3468d78326a833
Maharshee roy <m.roy@optonline.net>
2018-11-05 19:46:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+def batch_badge_creator(array)
+  return array
+end
+def assign_rooms(array)
+  counter = 1 
+  while counter < 8
+  puts "Hello, #{array[counter]}! You'll be assigned to room #{counter}!"
+  counter += 1 
+end
+end
